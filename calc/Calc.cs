b7acdc0ab17d73fb4e6ed836fde3d0fb3110c561
ruskikoreec <113416751+ruskikoreec@users.noreply.github.com>
2023-03-30 14:48:01 +0300

    First commit

---
+++ calc/Calc.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    // сделать историю линкед листом в pult в calc методы с 1 числом убрать introduced 
+    interface Icommand
+    {
+        int Positive(int Introduced);
+        int Negative(int Introduced);
+    }
+    interface IChange
+    {
+        void Change(int current);
+    }
+    class Calc
+    {
+        public int Current { get; set; } = 0;
+
+        public int Plus(int Introduced)
+        {
+            Current = Current+Introduced;
+            return Current;
+        }
+        public int Minus(int Introduced)
+        {
+            Current = Current - Introduced;
+            return Current;
+        }
+        public int Multiplication(int Introduced)
+        {
+            Current = Current*Introduced;
+            return Current;
+        }
+        public int Division(int Introduced)
+        {
+            Current = Current / Introduced;
+            return Current;
+        }
+        public void ChangeCurrent(int current)
+        {
+            Current = current;
+        }
+
+        
+    }
+    class CalcSimpleCommand:Icommand
+    {
+        private Calc calc;
+        public CalcSimpleCommand(Calc calc) => this.calc = calc;
+        public int Positive(int Introduced) => calc.Plus(Introduced);
+        public int Negative(int Introduced) => calc.Minus(Introduced);
+    }
+    class CalcHardCommand:Icommand
+    {
+        private Calc calc;
+        public CalcHardCommand(Calc calc) => this.calc = calc;
+        public int Positive(int Introduced) => calc.Multiplication(Introduced);
+        public int Negative(int Introduced) => calc.Division(Introduced);
+    }
+    class CalcCancel:IChange
+    {
+        private Calc calc;
+        public CalcCancel(Calc calc) => this.calc = calc;
+        public void Change(int current) => calc.ChangeCurrent(current);
+
+    }
+    class pult
+    {
+        private List<Icommand> commands;
+        private List<IChange> changes;
+        private List<int> History;
+        private int current { get; set; } = 0;
+
+        public int CalcSimpleCommands { get; set; }
+        public int CalcHardCommands { get; set; }
+        public int Introduced { get; set; }
+        public int result { get; set; }
+
+        public pult()
+        {
+            commands = new List<Icommand>() { null, null };
+            changes = new List<IChange>() { null, null };
+            History = new List<int>();
+            History.Add(0);
+        }
+        public void SetCommand(int button,Icommand command) => commands[button] = command;
+        public void SetChange(int button, IChange Change) => changes[button] = Change;
+
+        public void PressPlusMinus(int button)
+        {
+            for (int i = History.Count - 1; i > current; i--)
+            {
+                History.RemoveAt(i);
+            }
+            if (button == 1)
+            {
+                result = commands[0].Positive(Introduced);
+                History.Add(result);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                result = commands[0].Negative(Introduced);
+                History.Add(result);
+                Console.WriteLine(result);
+            }
+            current++;
+        }
+        public void PressMulDiv(int button)
+        {
+            for (int i = History.Count-1;i>current;i--)
+            {
+                History.RemoveAt(i);
+            }
+            if (button == 1)
+            {
+                result = commands[1].Positive(Introduced);
+                History.Add(result);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                result = commands[1].Negative(Introduced);
+                History.Add(result);
+                Console.WriteLine(result);                
+            }
+            current++;
+        }
+        public void PressCancel(int button)
+        {
+
+            if (button == 1)
+            {
+                Console.WriteLine(History[current-1]);
+                changes[0].Change(History[current - 1]);
+                current--;
+                //undo
+            }
+            else
+            {
+                Console.WriteLine(History[current+1]);
+                changes[0].Change(History[current + 1]);
+                current++;
+            }
+        }
+    }
+}
---
+++ calc/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //0 redo 1 undo   // 0 minus 1 plus 
+            Calc calc = new Calc();
+            calc.Current = 0;
+            pult pult = new pult();
+            pult.CalcSimpleCommands = 0;
+            pult.CalcHardCommands = 1;
+            pult.SetCommand(0, new CalcSimpleCommand(calc));
+            pult.SetCommand(1, new CalcHardCommand(calc));
+            pult.SetChange(0, new CalcCancel(calc));
+            Console.WriteLine("0");
+            string Oper= Console.ReadLine();
+            while(Oper!="")
+            {
+                string Number = " ";
+                switch (Oper)
+                {
+                    case "+":
+                        Number = Console.ReadLine();
+                        pult.Introduced = int.Parse(Number);
+                        pult.PressPlusMinus(1);
+                        break;
+                    case "-":
+                        Number = Console.ReadLine();
+                        pult.Introduced = int.Parse(Number);
+                        pult.PressPlusMinus(0);
+                        break;
+                    case "*":
+                        Number = Console.ReadLine();
+                        pult.Introduced = int.Parse(Number);
+                        pult.PressMulDiv(1);
+                        break;
+                    case "/":
+                        Number = Console.ReadLine();
+                        pult.Introduced = int.Parse(Number);
+                        pult.PressMulDiv(0);
+                        break;
+                    case "undo":
+                        pult.PressCancel(1);
+                        break;
+                    case "redo":
+                        pult.PressCancel(0);
+                        break;
+                }
+                Oper = Console.ReadLine();
+            }
+            
+        }
+    }
+}
